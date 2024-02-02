86f8f5d8b4789c9f3980932b466a79ce009cfb77
christian-gerard <1@christiangerard.me>
2024-02-01 20:28:46 -0700

    FINAL

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  const books = fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then((book) => renderBooks(book))
+
   
+  
+  console.log(books)
+  return books
 }
 
 function renderBooks(books) {
@@ -13,5 +20,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+
+  fetchBooks()
 });
